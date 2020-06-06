7acfbbab0480678c0d90cf45882f735bf24f7f5a
Julio92-C <juliocesar921016@gmail.com>
2020-06-06 08:35:57 +0800

    My First App

---
+++ send_mail.py
@@ -0,0 +1,21 @@
+import smtplib
+from email.mime.text import MIMEText
+
+def send_mail(customer, dealer, rating, comments):
+    port = 2525
+    smtp_server = 'smtp.mailtrap.io'
+    login = 'a8cfa54676ad2b'
+    password = 'c1accdb36d2f24'
+    message = f"<h3>New Feedback Submission</h3><ul><li>Customer: {customer}</li><li>Dealer: {dealer}</li><li>Rating: {rating}</li><li>Comments: {comments}</li></ul>"
+
+    sender_email = '54b9dd2f7f-1e78bd@inbox.mailtrap.io'
+    receiver_email = '54b9dd2f7f-1e78bd@inbox.mailtrap.io'
+    msg = MIMEText(message, 'html')
+    msg['Subject'] = 'Lexus Feedback'
+    msg['From'] = sender_email
+    msg['To'] = receiver_email
+
+    #Send email
+    with smtplib.SMTP(smtp_server, port) as server:
+        server.login(login, password)
+        server.sendmail(sender_email, receiver_email, msg.as_string())
